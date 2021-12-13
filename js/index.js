1f8e3a4407ccb2ea204eb7655cb7f30c8b5a5540
unknown <mauriaucourt.sarah@gmail.com>
2021-12-13 20:27:08 +0100

    responsive footer + infos pratiques

---
+++ js/index.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+
+    $("#open").click(function(){
+        $("header nav").addClass("open");
+    });
+
+    $("#close").click(function(){
+        $("header nav").removeClass("open");
+    });
+});
