eb817e2c0b1304040c2438a9b9e28fff126e4fb1
unknown <mauriaucourt.sarah@gmail.com>
2021-12-18 12:20:49 +0100

    menu burger

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 jQuery(document).ready(function ($) {
-
-	$("#open").click(function () {
-		$("header nav").addClass("open");
-	});
-
-	$("#close").click(function () {
-		$("header nav").removeClass("open");
-	});
+	if (window.matchMedia("(max-width: 768px)").matches) {
+		// état à l'ouverture du menu
+		$("#open").click(function () {
+			$("header nav").addClass("open")
+			$("header nav ul").css({
+				"opacity": "1",
+			});
+		});
+		// état à la fermeture du menu
+		$("#close, header ul li a").click(function () {
+			$("header nav").removeClass("open");
+			$("header nav ul").css({
+				"opacity": "0"
+			});
+		});
+	}
 });
