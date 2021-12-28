3720745aaf254ffcda3ce25eaabfe061b2692dd4
unknown <mauriaucourt.sarah@gmail.com>
2021-12-28 12:24:06 +0100

    improve responsive

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function ($) {
+
+	// Il est nécessaire de recharger la page, lorsqu'on teste le responsive et qu'on passe au-dessus ou en dessous d'une largeur de 768 px.  
 	if (window.matchMedia("(max-width: 768px)").matches) {
+
 		// état à l'ouverture du menu
 		$("#open").click(function () {
 			$("header nav").addClass("open")
@@ -7,6 +10,7 @@ jQuery(document).ready(function ($) {
 				"opacity": "1",
 			});
 		});
+
 		// état à la fermeture du menu
 		$("#close, header ul li a").click(function () {
 			$("header nav").removeClass("open");
